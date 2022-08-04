96e86783568ef277ef6c2c52f4b4ac5d46a9a03c
Royce Strange <roycest@swbell.net>
2022-08-04 14:10:59 -0500

    fix port

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -38,6 +39,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
